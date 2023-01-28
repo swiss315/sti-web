486bdf544314a98f0e1fafac23c75ce241ecd417
swiss315 <habeeb.sunmonud@gmail.com>
2023-01-28 19:37:25 +0100

    dashboard layout update

--- src/pages/Dashboardlayout.js
+++ src/pages/Dashboardlayout.js
@@ -4,10 +4,13 @@ import { Route, Routes } from "react-router-dom";
 import '../stylesheets/dashboardlayout.css'
 
 import Dashboard from './Dashboard';
+import Sidebar from '../components/Sidebar';
 
 import {ReactComponent as Logoicon} from "../assets/icons/logoicon.svg";
 import {ReactComponent as Closeicon} from "../assets/icons/closeicon.svg";
 import {ReactComponent as Sidebarmenu} from "../assets/icons/sidebarmenu.svg";
+import {ReactComponent as Labelname} from "../assets/icons/blacklabel.svg";
+import {ReactComponent as Notification} from "../assets/icons/notification.svg";
 
 function Dashboardlayout() {
   const [sidebar, setSidebar] = useState(false);
@@ -24,11 +27,17 @@ function Dashboardlayout() {
                 <Logoicon />
                 <Closeicon className='closeicon' onClick={handleclick} />
             </div>
-
+            <Sidebar />
         </div>
         <div className={` ${sidebar ? "maincontentexpand" : "maincontent"}`}>
-          <div className='sidebar-container'>
-            <Sidebarmenu onClick={handleclick}/>
+          <div className='dashboard-nav'>
+            <div className='sidebar-container'>
+              <Sidebarmenu onClick={handleclick}/>
+            </div>
+            <div className='profile-notification'>
+              <Labelname />
+              <Notification />
+            </div>
           </div>
 
           <Routes>
