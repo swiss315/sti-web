b571b897852532ffd018e4f93f39e77ff6651d54
swiss315 <habeeb.sunmonud@gmail.com>
2023-01-30 18:02:08 +0100

    dashboard update

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import '../stylesheets/dashboard.css'
+
+import marineinsurance from "../assets/images/marineinsurance.png";
+import {ReactComponent as Searchicon} from "../assets/icons/searchicon.svg";
 
 function Dashboard() {
+  const [tab, setTab] = useState("Motor")
   return (
-    <div>Dashboard</div>
+    <div className='dashboard-content'>
+      <p>
+        Hi <span>Khalid</span>,
+      </p>
+      <h2 className='dashboard-greetings'>
+        Good Morning!
+      </h2>
+      <div className='search-container'>
+        <div className='input-group'>
+            <label>
+                <Searchicon />
+                <span>
+                Search
+                </span>
+            </label>
+            <input name='firstname' type='text' className='' />
+        </div>
+      </div>
+      <div className='insurance-tab-container'>
+        <div className={`insurance-type-tab ${tab === "Motor" ? "active" : ""}`} onClick={() => {
+              setTab("Motor");
+            }}>
+          Motor Insurance
+        </div>
+        <div className={`insurance-type-tab ${tab === "Risk" ? "active" : ""}`} onClick={() => {
+              setTab("Risk");
+            }}>
+          All Risk Insurance
+        </div>
+        <div className={`insurance-type-tab ${tab === "Swiss" ? "active" : ""}`} onClick={() => {
+              setTab("Swiss");
+            }}>
+          Swiss-F Insurance
+        </div>
+        <div className={`insurance-type-tab ${tab === "Travel" ? "active" : ""}`} onClick={() => {
+              setTab("Travel");
+            }}>
+          Easy Travel Insurance Cover
+        </div>
+        <div className={`insurance-type-tab ${tab === "Marine" ? "active" : ""}`} onClick={() => {
+              setTab("Marine");
+            }}>
+          Marine Insurance
+        </div>
+        <div className={`insurance-type-tab ${tab === "Contractor" ? "active" : ""}`} onClick={() => {
+              setTab("Contractor");
+            }}>
+          Contractors All Risk Insurance
+        </div>
+      </div>
+      <div className='insurance-tab-content'>
+          <div className='tab-box' style={{ backgroundImage: `url(${marineinsurance})` }}>
+            <div>
+              <h2>
+                Marine Insurance
+              </h2>
+              <p>
+                2 policies
+              </p>
+            </div>
+          </div>
+          <div className='tab-box' style={{ backgroundImage: `url(${marineinsurance})` }}>
+            <div>
+              <h2>
+                Marine Insurance
+              </h2>
+              <p>
+                2 policies
+              </p>
+            </div>
+          </div>
+          <div className='tab-box' style={{ backgroundImage: `url(${marineinsurance})` }}>
+            <div>
+              <h2>
+                Marine Insurance
+              </h2>
+              <p>
+                2 policies
+              </p>
+            </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
